1af83baa6e1a46ad89e9923f030869906aab57f7
Vinicius Assis <viniciusassis2002@gmail.com>
2025-03-10 17:45:34 -0300

    Add folder

---
+++ Primeiros-passos/src/main/java/dio/Primeiros_passos/PrimeirosPassosComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dio.Primeiros_passos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosComSpringBootApplication.class, args);
+	}
+
+}
---
+++ Primeiros-passos/src/test/java/dio/Primeiros_passos/PrimeirosPassosComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.Primeiros_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
